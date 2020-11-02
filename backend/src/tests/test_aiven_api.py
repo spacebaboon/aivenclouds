16ba041f5184629f9434821918a21029a5205867
Ben <spacebaboon@gmail.com>
2020-11-02 11:23:41 +0100

    Add api tests
    Add test to show that development mode uses dummy data.
    Add (failing) test to show that cacheing avoids multiple API calls.

--- backend/src/tests/test_aiven_api.py
+++ backend/src/tests/test_aiven_api.py
@@ -1,21 +1,50 @@
+import unittest
 import responses
-import test_data
+from dummy_data import api_data
 from aiven_api import get_cloud_list
 
 
-@responses.activate
-def test_fetches_api_data():
-    responses.add(
-        responses.GET,
-        "https://api.aiven.io/v1/clouds",
-        json=test_data.clouds,
-        status=200,
-    )
+class TestJsonFormatting(unittest.TestCase):
+    @responses.activate
+    def test_fetches_api_data(self):
+        responses.add(
+            responses.GET,
+            "https://api.aiven.io/v1/clouds",
+            json=api_data.get("clouds"),
+            status=200,
+        )
 
-    get_cloud_list()
+        get_cloud_list("production")
 
-    assert len(responses.calls) == 1
+        self.assertEqual(len(responses.calls), 1)
+
+    @responses.activate
+    def test_returns_dummy_data_in_development_mode(self):
+        responses.add(
+            responses.GET,
+            "https://api.aiven.io/v1/clouds",
+            json=api_data.get("clouds"),
+            status=200,
+        )
+
+        get_cloud_list("development")
+
+        self.assertEqual(len(responses.calls), 0)
+
+    @responses.activate
+    def test_caches_api_calls(self):
+        responses.add(
+            responses.GET,
+            "https://api.aiven.io/v1/clouds",
+            json=api_data.get("clouds"),
+            status=200,
+        )
+
+        get_cloud_list("production")
+        get_cloud_list("production")
+
+        self.assertEqual(len(responses.calls), 1)
 
 
 if __name__ == "__main__":
-    test_fetches_api_data()
+    unittest.main()
